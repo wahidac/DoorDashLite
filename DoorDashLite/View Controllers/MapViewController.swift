73037c4c6ba83dd3890589eb44f1a616a6f31f64
Wahid <wahidac@gmail.com>
2018-11-25 18:43:38 -0800

    clean this up

--- DoorDashLite/View Controllers/MapViewController.swift
+++ DoorDashLite/View Controllers/MapViewController.swift
@@ -14,7 +14,7 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var currentAddress: UILabel!
     
-    private var currentPosition: MKPointAnnotation!
+    var currentPosition: MKPointAnnotation!
     var delegate: MapViewControllerDelegate?
     
     override func viewDidLoad() {
@@ -32,19 +32,11 @@ class MapViewController: UIViewController {
         mapView.addAnnotation(currentPosition)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("I have appeared!")
-    }
-    
     @IBAction func confirmAddressTapped(_ sender: Any) {
         delegate?.locationSelected(location: mapView.centerCoordinate)
     }    
 }
 
-// Pin looks like ass how can we customize? look into viewForAnnotation and returning MKPinAnnotationView
-// Will need to maybe do this so user cant touch annotation and fuck w/ the view. make sure you look into
-// the reuse identifier
 extension MapViewController: MKMapViewDelegate {
     func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
         // Update the current position
